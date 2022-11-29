aa823e46c9f3e75705547ee3ce1e6ca94daf32d8
Rafael Vaz <eurafavaz@gmail.com>
2022-11-29 20:32:34 -0300

    feat: add prisma client and postgreesql

---
+++ prisma/migrations/20221129233100_backendproject2/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_id_key" ON "user"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_cpf_key" ON "user"("cpf");
